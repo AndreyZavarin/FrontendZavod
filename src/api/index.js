02adde573cc2887cab522b57ef7f8428d120699d
Zavarin Andrey <zawarin.andrey20@yandex.ru>
2017-04-24 23:14:33 +0300

    Fix api

--- src/api/index.js
+++ src/api/index.js
@@ -1,41 +1,25 @@
 import fetch from 'isomorphic-fetch'
-/* global API_URL */
-
-// export const post = (url, data) => {
-//   console.debug("send post request on: " + url)
-//   return fetch(url, {
-//     method: 'POST',
-//     mode: 'no-cors',
-//     headers: {
-//       'Accept': 'application/json;charset=UTF-8',
-//     },
-//     body: JSON.stringify(data),
-//   }).then(response => response.json())
-// }
 
 export const get = (url) => {
-  console.debug("send get request on: " + url)
-  return fetch(url)
-    .then(response => {
-      if (response.status === 401) {
-        //browserHistory.push('/login')
-        const json = response.json()
-        throw new Error(json.error)
-      }
-      return response.json()
-    })
+    console.debug("send get request on: " + url)
+    return fetch(API_URL + url)
+        .then(response => {
+            if (response.status === 401) {
+                //browserHistory.push('/login')
+                const json = response.json()
+                throw new Error(json.error)
+            }
+            return response.json()
+        })
 }
-// export const post = (url, data) => {
-//     console.debug("send post request on: " + url)
-//     return fetch(url, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json;charset=UTF-8'
-//         },
-//         body: JSON.stringify(data)
-//     }).then(response => response.json())
-// }
 
-
-
-
+export const post = (url, data) => {
+    console.debug("send post request on: " + url)
+    return fetch(API_URL + url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify(data),
+    }).then(response => response.json())
+}
