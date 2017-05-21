72117a92a50b6f0c2d2a8faadce88dfc0750e307
Zavarin Andrey <zawarin.andrey20@yandex.ru>
2017-05-21 15:00:18 +0300

    Add component EditingClient from test data

--- src/components/Clients/EditingClient.js
+++ src/components/Clients/EditingClient.js
@@ -18,39 +18,42 @@ export default class EditingClient extends Component {
 
         return <div>
             <div className="col-sm-12" style={{background: '#ffffff'}}>
-                <h4>Редактирование клиента</h4>
+                <ul className="breadcrumb" style={{background: '#fff', fontSize: '16px'}}>
+                    <li><a style={{ color: '#666666'}} onClick={this.props.updateActivePage.bind(this, 'clients')} href="#">Клиенты</a> <span className="divider"></span></li>
+                    <li className="active"><span className="glyphicon glyphicon-edit"></span> Редактирование клиента</li>
+                </ul>
             </div>
             <div className="col-sm-12">
 
-            <form className="col-sm-offset-2 form-horizontal">
+            <form className="col-sm-offset-2 form-horizontal" style={{marginTop: '5%'}}>
                 <div className="form-group">
                     <label for="inputEmail3" className="col-sm-2 control-label"  >Имя</label>
                     <div className="col-sm-4">
-                        <input type="email" className="form-control" id="inputEmail3" placeholder="Имя" value={editingClientData.firstName} />
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="Имя" value="Сергей"/>
                     </div>
                 </div>
                 <div className="form-group">
                     <label for="inputEmail3" className="col-sm-2 control-label">Фамилия</label>
                     <div className="col-sm-4">
-                        <input type="email" className="form-control" id="inputEmail3" placeholder="Фамилия"/>
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="" value="Иванов"/>
                     </div>
                 </div>
                 <div className="form-group">
                     <label for="inputEmail3" className="col-sm-2 control-label">Отчество</label>
                     <div className="col-sm-4">
-                        <input type="email" className="form-control" id="inputEmail3" placeholder="Отчество"/>
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="" value="Отчество"/>
                     </div>
                 </div>
                 <div className="form-group">
                     <label for="inputEmail3" className="col-sm-2 control-label">Адрес</label>
                     <div className="col-sm-4">
-                        <input type="email" className="form-control" id="inputEmail3" placeholder="Адрес"/>
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="Адрес" value="г. Пенза, ул. Плеханова 4а"/>
                     </div>
                 </div>
                 <div className="form-group">
                     <label for="inputEmail3" className="col-sm-2 control-label">Дата рождения</label>
                     <div className="col-sm-4">
-                        <input type="date" className="form-control" id="inputEmail3" />
+                        <input type="date" className="form-control" id="inputEmail3" value="15.01.17" />
                     </div>
                 </div>
                 <div className="form-group">
@@ -58,7 +61,7 @@ export default class EditingClient extends Component {
                     <div className="col-sm-4">
                         <div className="radio">
                             <label class="radio-inline">
-                                <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/> Мужской
+                                <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" checked="true"/> Мужской
                             </label>
                             <label class="radio-inline">
                                 <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/> Женский
@@ -69,21 +72,20 @@ export default class EditingClient extends Component {
                 <div className="form-group">
                     <label for="inputEmail3" className="col-sm-2 control-label">Телефон</label>
                     <div className="col-sm-4">
-                        <input type="email" className="form-control" id="inputEmail3" placeholder="Телефон"/>
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="Телефон" value="8-937-212-33-44"/>
                     </div>
                 </div>
                 <div className="form-group">
                     <label for="inputEmail3" className="col-sm-2 control-label">Эл. почта</label>
                     <div className="col-sm-4">
-                        <input type="email" className="form-control" id="inputEmail3" placeholder="Эл. почта"/>
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="Эл. почта" value="avan@mail.ru"/>
                     </div>
                 </div>
 
                 <div className="form-group">
-                    <div className="col-sm-offset-2 col-sm-10">
-                        <button type="submit" className="btn btn-success" >Добавить клиента</button>
-                        <button type="submit" className="btn btn-success">Добавить и пройти анкетирование</button>
-                        <button type="submit" className="btn btn-default">Отмена</button>
+                    <div className="col-sm-offset-2 col-sm-10" style={{marginTop: '80px'}}>
+                        <button type="submit" className="btn btn-success" style={{margin:'0 10px 0 10px'}}>Сохранить</button>
+                        <button type="submit" className="btn btn-default" style={{margin:'0 10px 0 10px'}}>Отмена</button>
                     </div>
                 </div>
             </form>
